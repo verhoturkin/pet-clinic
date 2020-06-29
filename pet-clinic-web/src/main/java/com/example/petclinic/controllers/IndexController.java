c5e5c3645098f306679ce2e34b91200bec2843d0
Verkhoturkin <verhoturkin@gmail.com>
2020-06-29 17:26:31 +0500

    add index page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.example.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created 29/06/2020 - 17:19
+ *
+ * @author Alexander Verkhoturkin
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
