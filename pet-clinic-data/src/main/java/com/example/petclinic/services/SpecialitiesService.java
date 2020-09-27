b2e231f6ce4a3a7e49978f4e7396b6f0e88e1ef4
Alexander <verhoturkin@gmail.com>
2020-09-27 18:48:32 +0500

    Create Speciality Map service impl. closes #30

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,11 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Speciality;
+
+/**
+ * created 27.09.2020 - 18:43
+ *
+ * @author Alexander Verkhoturkin
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
