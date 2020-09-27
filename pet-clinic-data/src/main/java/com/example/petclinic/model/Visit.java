81921920fc5c7749ea8f8f446870ea57d7249edb
Alexander <verhoturkin@gmail.com>
2020-09-27 17:42:53 +0500

    add visit entity. closes #25

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
@@ -0,0 +1,39 @@
+package com.example.petclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * created 27.09.2020 - 17:37
+ *
+ * @author Alexander Verkhoturkin
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
